55e18e932e9c343293f6dfa6e7f590ab54bd9409
sm2011 <23eagle@live.com>
2015-03-05 12:07:59 +0100

    testing

---
+++ Graph/Edge.java
@@ -0,0 +1,22 @@
+public class Edge {
+
+	private int weight;
+	private Node endNode;
+
+	public int getWeight() {
+		return 0;
+	}
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+	public Node getEndNode() {
+		return endNode;
+	}
+
+	public void setEndNode(Node endNode) {
+		this.endNode = endNode;
+	}
+
+}
---
+++ HashFunction/test2.java
@@ -0,0 +1,17 @@
+import java.util.Hashtable;
+
+
+
+
+public class test2 {
+	
+	public static void main(String[] args) {
+		
+		Hashtable<Integer, Integer> table = new Hashtable<Integer, Integer>();
+		
+		for (int i = 0; i < args.length; i++) {
+			
+		}
+	}
+
+}
